6abcec8e04b7bc35ecd51628b2de4cb463c9acfe
nag763 <loic.labeye@tutanota.de>
2025-03-26 17:59:41 +0100

    Rename keypair

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_vpc_security_group_egress_rule" "outbound" {
 }
 
 # Step 6: Create a Key Pair for SSH Access
-resource "aws_key_pair" "example" {
+resource "aws_key_pair" "vps" {
   key_name   = "examplekey"
   public_key = file("~/.ssh/terraform.pub")
 }
@@ -100,7 +100,7 @@ resource "aws_key_pair" "example" {
 resource "aws_instance" "ec2" {
   ami                         = "ami-0160e8d70ebc43ee1"
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.example.key_name
+  key_name                    = aws_key_pair.example.vps
   vpc_security_group_ids      = [aws_security_group.allow_tls.id]
   subnet_id                   = aws_subnet.main.id
   associate_public_ip_address = true
